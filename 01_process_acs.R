b4fe839d15dd624741ae9649248be29ef9ab1b70
vegalla <70249519+vegalla@users.noreply.github.com>
2021-12-13 01:52:22 -0500

    Add norfolk data and code

--- 01_process_acs.R
+++ 01_process_acs.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(janitor)
 library(purrr)
 
-## Process Data -------------------------------------------------------------------
+## Process DC Data -------------------------------------------------------------
 
 # Income
 
@@ -14,7 +14,7 @@ dc_income <-
       
       ## Set data path
       
-      path = paste0('project/_data/raw/income/ACSST5Y',as.character(x),'.S1901_data_with_overlays_2021-12-01T145310.csv')
+      path = paste0('data/raw/acs/dc/income/ACSST5Y',as.character(x),'.S1901_data_with_overlays_2021-12-01T145310.csv')
       
       ## Read raw data
       
@@ -55,7 +55,7 @@ dc_house_value <-
       
       ## Set data path
 
-      path = paste0('project/_data/raw/house_value/ACSST5Y', 
+      path = paste0('data/raw/acs/dc/house_value/ACSST5Y', 
                     as.character(x),
                     '.S2506_data_with_overlays_2021-12-01T151340.csv')
       
@@ -102,7 +102,7 @@ dc_social <-
       
       ## Set data path
       
-      path = paste0('project/_data/raw/social/ACSDP5Y', 
+      path = paste0('data/raw/acs/dc/social/ACSDP5Y', 
                     as.character(x),
                     '.DP02_data_with_overlays_2021-11-29T151702.csv')
       
@@ -125,7 +125,14 @@ dc_social <-
         # DP02_0088PE - Born in us 
         # DP02_0111PE - Speaks only English
         
-        select(geo_id, dp02_0007pe, dp02_0009pe, dp02_0039e, dp02_0064pe, dp02_0088pe, dp02_0111pe) %>%
+        select(
+          geo_id, 
+          dp02_0007pe, 
+          dp02_0009pe, 
+          dp02_0039e, 
+          dp02_0064pe, 
+          dp02_0088pe, 
+          dp02_0111pe) %>%
       
         
         ## Filter out first observation (labels)
@@ -144,13 +151,14 @@ dc_social <-
           pct_born_us = as.numeric(dp02_0088pe), 
           pct_only_speaks_english = as.numeric(dp02_0111pe)) %>%
           
-        select(GEOID, year, 
-               pct_single_father, 
-               pct_single_mother, 
-               fertility, 
-               pct_bachelors,
-               pct_born_us,
-               pct_only_speaks_english)
+        select(
+          GEOID, year, 
+          pct_single_father, 
+          pct_single_mother, 
+          fertility, 
+          pct_bachelors,
+          pct_born_us,
+          pct_only_speaks_english)
       
     }) %>% 
   bind_rows()
@@ -171,8 +179,8 @@ dc_features <-
 # Set to False to prevent overwriting
 export = FALSE
 
-if (export = TRUE) {
+if (export == TRUE) {
   dc_features %>%
-    write_csv('project/_data/processed/dc_features.csv')
+    write_csv('data/processed/dc_features.csv')
 }
 
--- 02_merge_data.R
+++ 02_merge_data.R
@@ -1,11 +1,14 @@
 ## Set up ----------------------------------------------------------------------
 library(sf)
 library(tidyverse)
+library(rmapshaper)
 
 ## Load data -------------------------------------------------------------------
 
+# DC
+
 dc_shp <-
-  st_read('data/raw/spatial/census/census.shp') %>%
+  st_read('data/raw/spatial/census.shp') %>%
   
   # Filter to DC shape
   
@@ -23,13 +26,54 @@ dc_shp <-
   
   rmapshaper::ms_simplify(keep = 0.10)
 
-label_ncrc_gent <-
+# Norfolk, Virginia
+
+va_shp <-
+  st_read('data/raw/spatial/census.shp') %>%
+  
+  # Filter to Norfolk, VA shape, subtract GEOID in ocean
+  
+  filter(state_name == 'Virginia',
+         COUNTYFP == '710',
+         GEOID != 51710990000) %>%
+  
+  # Keep only GEOID and geometry
+  
+  dplyr::select(GEOID) %>%
+  
+  # Project to CRS 4326
+  
+  st_transform(crs = 4326) %>% 
+  
+  # Simplify shape to reduce size
+  
+  rmapshaper::ms_simplify(keep = 0.10)
+
+## Load gentrification data from NCRC interactive map --------------------------
+
+# DC
+
+dc_label <-
   read_csv(
-    'project/_data/raw/label/ncrc_dc_gentrification.csv',
+    'data/raw/label/ncrc_dc_gentrification.csv',
     col_types =  cols(
       GEOID = col_character(),
       gentrified = col_double(),
       year_gentrified = col_double()))
+
+# VA
+
+va_label <-
+  read_csv(
+    'data/raw/label/ncrc_va_gentrification.csv',
+    col_types =  cols(
+      GEOID = col_character(),
+      gentrified = col_double(),
+      year_gentrified = col_double()))
+
+## Load ACS features data ------------------------------------------------------
+
+# DC
 
 dc_features <-
   read_csv(
@@ -39,26 +83,39 @@ dc_features <-
 
 ## Merge data to Census Tract --------------------------------------------------
 
+# DC
+
 dc <-
   dc_shp %>%
     left_join(
       dc_features,
       by = 'GEOID') %>%
     left_join(
-      label_ncrc_gent %>%
+      dc_label %>%
         filter(year_gentrified == 2017) %>%
         select(GEOID, gentrified),
       by = 'GEOID') %>%
   mutate(gentrified = replace_na(gentrified, 0))
 
-export_dc = FALSE
+# VA
 
-if (export_DC = TRUE){
+va <-
+  va_shp %>%
+  left_join(
+    va_label %>%
+      select(GEOID, gentrified),
+    by = 'GEOID') %>%
+  mutate(gentrified = replace_na(gentrified, 0))
+
+
+## Export data -----------------------------------------------------------------
+export = FALSE
+
+if (exportC == TRUE){
   dc %>%
     st_write('project/_data/processed/dc_processed.shp')
 } 
 
-## Clean missing data ?? -------------------------------------------------------
 
 ## KerasR
 # https://cran.r-project.org/web/packages/kerasR/vignettes/introduction.html
